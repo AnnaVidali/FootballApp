dac7561f83de06fc0ac9c5393389d11b3e2ee6cf
ANNA\annav <annavidali99@gmail.com>
2026-07-23 14:33:57 +0200

    fixed hamburger hidding main content on mobile

--- components/DashboardShell.tsx
+++ components/DashboardShell.tsx
@@ -95,7 +95,7 @@ export default function DashboardShell({
                 </div>
             </div>
             {/* Main content */}
-            <main className="flex-1 overflow-y-auto bg-gray-50 p-6">
+            <main className="flex-1 overflow-y-auto bg-gray-50 pt-16 pl-16 md:p-6">
                 {children}
             </main>
         </div>
